5ade27547b27fd4fb0cd7e0d0969699a3ac236f8
Seth Howard <smiles.seth@gmail.com>
2013-10-03 22:20:09 -0600

    added the instructions for the lucid challenge

--- lucidChallenge.js
+++ lucidChallenge.js
@@ -1,3 +1,40 @@
+// ===========
+// Find the length of the longest non-decreasing sequence through adjacent, non-repeating cells (including diagonals) in a rectangular grid of numbers in a language of your choice. The solution should handle grids of arbitrary width and height. 
+
+// For example, in the following grid, one legal path (though not the longest) that could be traced is 0->3->7->9 and its length would be 4. 
+
+// 8 2 4
+// 0 7 1
+// 3 7 9
+
+// The path can only connect adjacent locations (you could not connect 8 -> 9). The longest possible sequence for this example would be of length 6 by tracing the path 0->2->4->7->7->9 or 1->2->4->7->7->9.
+
+// Write a method, in a language of your choice, that takes a rectangular grid of numbers as input, and returns the length of the longest such sequence as output. 
+
+// Here are some example function signatures and calls in a few languages:
+
+// Javascript:
+// function longestSequence(grid) {
+// return 0;
+// }
+
+// longestSequence(
+// [[8,2,4]
+// ,[0,7,1]
+// ,[3,7,9]]
+// );
+
+// Java:
+// public static void main(String[] args) {
+// int[][] grid = {{8, 2, 4},
+// {0, 7, 1},
+// {3, 7, 9}};
+// System.out.println(longestSequence(grid));
+// }
+
+// private static int longestSequence(int[][] grid) {
+// return 0;
+// }
 
 function longestSequence(grid) {
     var answers = []
