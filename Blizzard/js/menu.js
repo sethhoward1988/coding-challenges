7ae878ec001219a971f8c7d0c1e54737522ffd41
Seth Howard <smiles.seth@gmail.com>
2013-11-06 15:58:28 -0700

    got a basic implementation working... need to style now

--- Blizzard/js/menu.js
+++ Blizzard/js/menu.js
@@ -1,10 +1,51 @@
 
-Menu = function () {
+Menu = (function () {
 
     var hash = {
 
-        initialize: function () {
-            console.log('Initializing new menu');
+        currentNodeDepth: 0,
+
+        initialize: function (json) {
+            this.data = json.menu;
+            this.el = document.createElement('div');
+            this.el.className = "menu"
+            this.render();
+        },
+
+        render: function () {
+            var menu = this.createSubMenu(this.el, this.data);
+            this.el.appendChild(menu);
+        },
+
+        createSubMenu: function (el, data, level) {
+            var menu = document.createElement('ul'),
+                length = data.length;
+
+            // Let's keep track of levels in case we want to target deeper menus with css
+            if(!level){
+                level = 0;
+            }
+
+            // Run through all the data at the current level
+            for(var i = 0; i < length; i++){
+                var currentData = data[i],
+                    item = document.createElement('li');
+
+                // Set the title of the current list item
+                item.innerText = currentData.title;
+
+                if(currentData.submenu){
+                    item.className = "submenu-" + level;
+
+                    // Recursively call this function to create all submenus found
+                    item.appendChild(this.createSubMenu(item, currentData.submenu, level + 1));
+                }
+
+                menu.appendChild(item);
+            }
+
+            return menu;
+
         }
 
     }
@@ -17,6 +58,6 @@ Menu = function () {
         Menu.prototype[prop] = hash[prop];
     }
 
-    return Funnelchart;
+    return Menu;
 
-}
+})();
